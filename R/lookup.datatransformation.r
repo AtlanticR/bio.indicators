e40fa801ef8bce02b4b933224739ac58cce88a8d
Jae S Choi <choijaeseok@gmail.com>
2016-06-16 12:58:57 -0300

    refactor

--- R/lookup.datatransformation.r
+++ R/lookup.datatransformation.r
@@ -7,14 +7,14 @@
       log.transform = variable.list.expand("log.transform")
       scaled.centered = variable.list.expand("scaled.centered")
       sn = variable.list.expand("all.data")
-      set = bio.snowcrab::snowcrab.db("set.merge.cat") # base transform characteristics
-      logs = bio.snowcrab::logbook.db('logbook')
+      set = bio.snowcrab::snowcrab.db(DS="set.merge.cat") # base transform characteristics
+      logs = bio.snowcrab::logbook.db(DS='logbook')
       repository = file.path( project.datadirectory("bio.snowcrab"), "R", "transform.lookup.rdata" )
     } else if (db=="groundfish") {
       log.transform = variable.list.expand("log.transform")
       scaled.centered = variable.list.expand("scaled.centered")
       sn = variable.list.expand("all")
-      set = bio.groundfish::groundfish.db("set" )
+      set = bio.groundfish::groundfish.db( DS="set" )
 	logs=NULL
       repository = file.path( project.datadirectory("bio.groundfish"), "R", "transform.lookup.rdata" )
     } else {
