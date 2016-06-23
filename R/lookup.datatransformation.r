76716fa85f6fa357d08c458a95794f7a8dbde467
Jae S Choi <choijaeseok@gmail.com>
2016-06-23 12:41:34 -0300

    move to bio.indicators

--- R/lookup.datatransformation.r
+++ R/lookup.datatransformation.r
@@ -4,16 +4,16 @@
     # determine data transformations based upon category of data and data source
 
     if (db=="snowcrab") {
-      log.transform = variable.list.expand("log.transform")
-      scaled.centered = variable.list.expand("scaled.centered")
-      sn = variable.list.expand("all.data")
+      log.transform = bio.snowcrab::variable.list.expand("log.transform")
+      scaled.centered = bio.snowcrab::variable.list.expand("scaled.centered")
+      sn = bio.snowcrab::variable.list.expand("all.data")
       set = bio.snowcrab::snowcrab.db(DS="set.merge.cat") # base transform characteristics
       logs = bio.snowcrab::logbook.db(DS='logbook')
       repository = file.path( project.datadirectory("bio.snowcrab"), "R", "transform.lookup.rdata" )
     } else if (db=="groundfish") {
-      log.transform = variable.list.expand("log.transform")
-      scaled.centered = variable.list.expand("scaled.centered")
-      sn = variable.list.expand("all")
+      log.transform = bio.groundfish::variable.list.expand("log.transform")
+      scaled.centered = bio.groundfish::variable.list.expand("scaled.centered")
+      sn = bio.groundfish::variable.list.expand("all")
       set = bio.groundfish::groundfish.db( DS="set" )
 	    logs=NULL
       repository = file.path( project.datadirectory("bio.groundfish"), "R", "transform.lookup.rdata" )
