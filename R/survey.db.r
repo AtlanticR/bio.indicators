fa6adb62a599fcc0e6febce6e9eccbdafd1dad07
Jae S Choi <choijaeseok@gmail.com>
2017-01-16 12:53:45 -0400

    mv lengthweight funcitonality to survey.db

--- R/survey.db.r
+++ R/survey.db.r
@@ -246,7 +246,9 @@
         lbm::array_map( "xy->1", set[,c("plon","plat")], gridparams=p$gridparams ), 
         lbm::array_map( "xy->1", bathymetry.db(p=p, DS="baseline"), gridparams=p$gridparams ) )
 
-      set = cbind( set, indicators.lookup( p=p, DS="spatial", locsmap=locsmap ) )
+      sp = indicators.lookup( p=p, DS="spatial", locsmap=locsmap )
+
+      set = cbind( set,  )
       set = cbind( set, indicators.lookup( p=p, DS="spatial.annual", locsmap=locsmap, timestamp=set[,"timestamp"] ))
       set$t = indicators.lookup( p=p, DS="temperature",   locsmap=locsmap, timestamp=set[,"timestamp"] )
 
@@ -254,6 +256,107 @@
       save( set, file=fn, compress=T )
       return (fn)
     }
+
+
+    # ---------------------
+
+    if (DS %in% c("lengthweight.redo", "lengthweight.parameters", "lengthweight.residuals") ) {
+
+      ## TODO -- make parallel require(multicore)
+
+      ddir = file.path( project.datadirectory("bio.indicators"), "data" )
+      dir.create( ddir, showWarnings=FALSE, recursive=TRUE )
+
+      fn = file.path( ddir, "bio.length.weight.parameters.rdata" )
+      fn2 = file.path( ddir, "bio.length.weight.residuals.rdata" )
+
+      if (DS=="lengthweight.parameters") {
+        res = NULL
+        if (file.exists( fn ) ) load( fn )
+        return( res )
+      }
+
+      if (DS=="lengthweight.residuals") {
+        lwr = NULL
+        if (file.exists( fn2 ) ) load( fn2 )
+        return( lwr )
+      }
+
+      # this mirrors the relevent changes/recoding in indicators.db("det")
+      x = survey.db( DS="det.init", p=p )
+      x$spec = x$spec_bio
+      x = x[ which( is.finite( x$spec)), ]
+      x$sex[ which( !is.finite(x$sex)) ] = 2 # set all uncertain sexes to one code sex code
+      x$mat[ which( !is.finite(x$mat)) ] = 2 # set all uncertain sexes to one code sex code
+
+      res = expand.grid(
+        spec = sort( unique( x$spec )),
+        sex = sort( unique( x$sex )),
+        mat = sort( unique( x$mat ))
+      )
+      # sex codes (snowcrab standard)
+      #  male = 0
+      #  female = 1
+      #  sex.unknown = 2
+
+      # mat codes (snowcrab standard)
+      #  imm = 0
+      #  mat = 1
+      #  mat.unknown = 2
+
+      unknown = 2
+      x$residual = NA
+
+      # initialise new variables
+      res$rsq = NA
+      res$sigma = NA
+      res$df = NA
+      res$b0 = NA
+      res$b1 = NA
+      res$b0.se = NA
+      res$b1.se = NA
+      res$pvalue = NA
+      for (i in 1:nrow(res)) {
+        wsp = which( x$spec == res$spec[i] )
+        if (length( wsp) < 10 ) next()
+        # remove extremes for each species from the data to generate regressions
+        ql = quantile( x$len[wsp], probs=c(0.005, 0.995), na.rm=T )
+        qm = quantile( x$mass[wsp], probs=c(0.005, 0.995), na.rm=T )
+        wqn =  which( x$len> ql[1] & x$len< ql[2] & x$mass> qm[1] & x$mass< qm[2] )
+        wsx = which( x$sex==res$sex[i] )
+        if (res$sex[i]==unknown) wsx = wsp  # use all possible data (not just unknowns) for this class (hermaphrodite/unsexed/unknown)
+        wmt = which( x$mat==res$mat[i] )
+        if (res$sex[i]==unknown) wsx = wsp  # use all possible data (not just unknowns) for this class (mat unkown)
+        w = intersect( intersect( intersect( wsp, wqn ), wsx ), wmt )
+        nw = length(w)
+        if ( nw > 5 ) {
+          q = x[w ,]
+          q.lm = try( lm( log10(mass) ~ log10(len), data=q ) )
+          if (class( q.lm) %in% "error" ) next()
+          s = summary( q.lm )
+          res$rsq[i] = s$r.squared
+          res$sigma[i] = s$sigma
+          res$df[i] = s$df[2]
+          res$b0[i] = s$coefficients[1]
+          res$b1[i] = s$coefficients[2]
+          res$b0.se[i] = s$coefficients[3]
+          res$b1.se[i] = s$coefficients[4]
+          res$pvalue[i] = pf(s$fstatistic[1],s$fstatistic[2],s$fstatistic[3],lower.tail=FALSE)
+          x$residual[w] = rstandard(q.lm)
+          print( res[i,] )
+        }
+      }
+      ooo = which( abs( x$residual ) > 4 )
+      if (length(ooo) > 0 ) x$residual [ooo] = NA
+      lwr = x
+      save( lwr, file=fn2, compress=TRUE )
+      save( res, file=fn, compress=TRUE )
+      return( fn )
+ 
+    }
+
+
+
 
 
     # --------------------
@@ -280,12 +383,12 @@
         #  mature = 1
         #  mat.unknown = 2
 
-      det = lengthweight.db( DS="residuals", p=p )
+      det = survey.db( DS="lengthweight.residuals", p=p )
 
       # fix mass, length estimates where possible using model parameters
       # try finest match first: by spec:mat, spec:sex, spec
 
-      lwp = lengthweight.db( DS="parameters", p=p )
+      lwp = survey.db( DS="lengthweight.parameters", p=p )
       # note: lwp$spec is derived from spec_bio, as above
 
       ims = which( !is.finite( det$mass) )
--- inst/scripts/01.indicators.r
+++ inst/scripts/01.indicators.r
@@ -3,11 +3,17 @@
   current.year = 2016
 
   ## NOTE resolution is fixed at SSE
+
+ # preform/prepare some lookup tables for faster lbm processing and generic lookups
+  p = bio.indicators::indicators.parameters( DS="indicators" )
+  indicators.db( DS="spatial.redo", p=p ) 
+  indicators.db( DS="spatial.annual.redo", p=p ) 
+  indicators.db( DS="spatial.annual.seasonal.redo", p=p ) 
+
   
   # ----------------------------------------------------------
   # glue biological data sets together from various surveys
-  p = bio.indicators::indicators.parameters( DS="survey" )
-    # load and glue data together
+   # load and glue data together
     if (redo.source.data) {
       # these are here to show the dependencies of survey.db()
       bio.groundfish::groundfish.db( "set.base.redo" )
@@ -18,25 +24,22 @@
       bio.snowcrab::snowcrab.db( DS ="det.georeferenced.redo" )
     }
 
+   p = bio.indicators::indicators.parameters( DS="survey" )
     survey.db( DS="set.init.redo", p=p )
     survey.db( DS="cat.init.redo", p=p )
     survey.db( DS="det.init.redo", p=p )
-    survey.db( DS="set.intermediate.redo", p=p ) # adds temperature required for metabolism lookup in "det.redo"
-    lengthweight.db( DS="update", p=p  )  # # TODO:: parallelize me ... update the lcoal tables (not necessary)
+
+    # the following requires the preformed indicators.db() (above) for lookups to complete the data
     survey.db( DS="det.redo", p=p ) # mass/length imputation and sanity checking
+    survey.db( DS="set.intermediate.redo", p=p ) # adds temperature required for metabolism lookup in "det.redo"
+    survey.db( DS="length.weight.redo", p=p  )  # # TODO:: parallelize me ... update the lcoal tables (not necessary)
     survey.db( DS="cat.redo", p=p ) # sanity checking and fixing mass estimates from det etc ...
     survey.db( DS="set.redo", p=p ) # mass/length imputation and sanity checking
     figure.bio.map.survey.locations()  # see mpa/src/_Rfunctions/figure.trawl.density for more control
    
 
  # -----------------------------
- # preform/prepare some lookup tables for faster lbm processing and generic lookups
-  p = bio.indicators::indicators.parameters( DS="indicators" )
-  indicators.db( DS="spatial.redo", p=p ) 
-  indicators.db( DS="spatial.annual.redo", p=p ) 
-  indicators.db( DS="spatial.annual.seasonal.redo", p=p ) 
   indicators.db( DS="prediction.surface.redo", p=p ) 
-
 
     if (0) {
       # not yet ready
