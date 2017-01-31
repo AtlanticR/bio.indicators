0c79c6c9933b66d7abf1c052630cc5e2b861a1d7
Jae S Choi <choijaeseok@gmail.com>
2017-01-31 18:08:54 -0400

    fix plotting method

--- R/indicators.map.R
+++ R/indicators.map.R
@@ -1,5 +1,5 @@
 
-indicators.map = function( ip=NULL, p=NULL, type="annual", vname=NULL, voi=NULL ) {
+indicators.map = function( ip=NULL, p=NULL, type="annual", voi=NULL ) {
 
   # ip is the first parameter passed in the parallel mode
   if (exists( "libs", p)) RLibrary( p$libs )
@@ -24,7 +24,7 @@ indicators.map = function( ip=NULL, p=NULL, type="annual", vname=NULL, voi=NULL 
       p1 = make.list( list( yrs=p1$yrs), Y=p1 )
       indicators.map( p=p1, type="lbm.stats" ) # no parallel option .. just a few
       indicators.map( p=p1, type="climatology" ) # no parallel option .. just a few
-      parallel.run( indicators.map, p=p1, type="annual", vname=bs )
+      parallel.run( indicators.map, p=p1, type="annual" )
     }
 
   }
@@ -35,6 +35,7 @@ indicators.map = function( ip=NULL, p=NULL, type="annual", vname=NULL, voi=NULL 
   if ( type %in% c("annual" ) ) {
     projectdir = file.path(p$project.root, "maps", voi, p$spatial.domain, "annual" )
     dir.create( projectdir, recursive=T, showWarnings=F )
+
     for (iy in ip ) {
       y = p$runs[iy, "yrs"]
       print(y)
@@ -91,44 +92,113 @@ indicators.map = function( ip=NULL, p=NULL, type="annual", vname=NULL, voi=NULL 
     projectdir = file.path( project.datadirectory("bio.temperature"), "maps", p$spatial.domain, "bottom.predictions", "climatology" )
     dir.create( bottomdir.maps, recursive=T, showWarnings=F )
 
-    H = indicators.db( p=p, DS="bottom.statistics.climatology" )
+    H = indicators.db( p=p, DS="complete" )
+    vnames = setdiff( names(H), c("plon", "plat" ))
+ 
+    for (vname in vnames ) {
 
-    for (vname in p$bstats) {
-      if (vname %in% c("tmean") ) {
-        # datacols = c("plon", "plat", "tmean")
-        datarange = seq(-0.5, 10, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "temperatures.bottom", sep=".")
-        annot = paste("Temperature bottom climatology\n", sep="")
+      if ( grepl("ca1", vname) ) {
+        datarange = seq(-2, 2, length.out=100)
+        annot = "CA1"
+      }
+
+      if ( grepl("ca2", vname) ) {
+        datarange = seq(-2, 2, length.out=100)
+        annot = "CA1"
       } 
 
-      if (vname %in% c("tsd") ) {
-        # datacols = c("plon", "plat", "tmean")
-        datarange = seq(0.001, 6, length.out=100)
+      if ( grepl("pca1", vname) ) {
+        datarange = seq(-2, 2, length.out=100)
+        annot = "PCA1"
+      }
+
+      if ( grepl("pca2", vname) ) {
+        datarange = seq(-2, 2, length.out=100)
+        annot = "PCA1"
+      }
+
+      if ( grepl("mean.climatology", vname) ) {
         cols = color.code( "blue.black", datarange )
-        outfn = paste( "temperatures.bottom.sd", sep=".")
-        annot = paste("Temperature bottom SD climatology\n", sep="")
+        outfn = paste( annot, "mean.climatology", sep=".")
+        annot = paste( annot, "Mean climatology\n", sep=" ")
+      } 
+
+      if ( grepl("sd.climatology", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "sd.climatology", sep=".")
+        annot = paste( annot, "SD climatology\n", sep=" ")
       } 
    
-      if (vname %in% c("tmin") ) {
-        datarange = seq(-0.5, 10, length.out=100)
+      if (grepl("sdTotal", vname) ) {
         cols = color.code( "blue.black", datarange )
-        outfn = paste( "temperatures.bottom.min", sep=".")
-        annot = paste("Temperature bottom minimum climatology\n",  sep="")
+        outfn = paste( annot, "sdTotal", sep=".")
+        annot = paste( annot, "sdTotal\n", sep=" ")
+        xyz=cbind(loc, H[,vname])
+      } 
+
+      if (grepl("rsquared", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "rsquared", sep=".")
+        annot = paste( annot, "rsquared\n", sep=" ")
+        xyz=cbind(loc, H[,vname])
+      } 
+   
+      if (grepl("ndata", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "ndata", sep=".")
+        annot = paste( annot, "ndata\n",  sep=" ")
+        xyz=cbind(loc, H[,vname])
       }
    
-      if (vname %in% c("tmax") ) {
-        datarange = seq(-0.5, 10, length.out=100)
+      if (grepl("sdSpatial", vname) ) {
         cols = color.code( "blue.black", datarange )
-        outfn = paste( "temperatures.bottom.max", sep=".")
-        annot = paste("Temperature bottom maximum climatology\n",  sep="")
+        outfn = paste( annot, "sdSpatial", sep=".")
+        annot = paste( annot, "sdSpatial\n",  sep=" ")
+        xyz=cbind(loc, H[,vname])
      }
 
-      if (vname %in% c("amplitude") ) {
-        datarange = seq(0,5, length.out=100)
+      if (grepl("sdObs", vname) ) {
         cols = color.code( "blue.black", datarange )
-        outfn = paste( "temperatures.bottom.amplitude", sep=".")
-        annot = paste("Temperature bottom amplitude climatology\n",  sep="")
+        outfn = paste( annot, "sdObs", sep=".")
+        annot = paste( annot, "sdObs\n",  sep=" ")
+        xyz=cbind(loc, (H[,vname]))
+      }
+
+      if (grepl("range", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "range", sep=".")
+        annot = paste( annot, "range\n",  sep=" ")
+        xyz=cbind(loc, log(H[,vname]))
+        xyz = xyz[which( is.finite(rowSums(xyz))),]
+      }
+
+      if (grepl("phi", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "phi", sep=".")
+        annot = paste( annot, "phi\n",  sep=" ")
+        xyz=cbind(loc, log(H[,vname]))
+        xyz = xyz[which( is.finite(rowSums(xyz))),]
+      }
+
+      if (grepl("nu", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "nu", sep=".")
+        annot = paste( annot, "nu\n",  sep=" ")
+        xyz=cbind(loc, (H[,vname]))
+      }
+
+      if (grepl("ar_timerange", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "ar_timerange", sep=".")
+        annot = paste( annot, "ar_timerange\n",  sep=" ")
+        xyz=cbind(loc, (H[,vname]))
+      }
+
+      if (grepl("ar_1", vname) ) {
+        cols = color.code( "blue.black", datarange )
+        outfn = paste( annot, "ar_1", sep=".")
+        annot = paste( annot, "ar_1\n",  sep=" ")
+        xyz=cbind(loc, H[,vname])
       }
 
       bio.spacetime::map( xyz=cbind(loc, H[,which( p$bstats==vname)]), cfa.regions=F, depthcontours=T, pts=NULL, annot=annot,
@@ -138,111 +208,6 @@ indicators.map = function( ip=NULL, p=NULL, type="annual", vname=NULL, voi=NULL 
     }
 
   }
-
-
-  # ------------------------------
-
-
-  if ( type %in% c("lbm.stats" ) ) {
-
-    bottomdir.maps = file.path( project.datadirectory("bio.temperature"), "maps", p$spatial.domain, "bottom.predictions", "lbm.stats" )
-    dir.create( bottomdir.maps, recursive=T, showWarnings=F )
-
-    H = indicators.db( p=p, DS="lbm.stats" )
-
-    for (vname in colnames(H)) {
-      
-      if (vname %in% c("sdTotal") ) {
-        datarange = seq(0.1, 6, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "sdTotal", sep=".")
-        annot = paste("Temperature bottom sdTotal\n", sep="")
-        xyz=cbind(loc, H[,vname])
-      } 
-
-      if (vname %in% c("rsquared") ) {
-        datarange = seq(0.1, 0.99, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "rsquared", sep=".")
-        annot = paste("Temperature bottom rsquared\n", sep="")
-        xyz=cbind(loc, H[,vname])
-      } 
-   
-      if (vname %in% c("ndata") ) {
-        datarange = seq(1, 8000, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "ndata", sep=".")
-        annot = paste("Temperature bottom ndata\n",  sep="")
-        xyz=cbind(loc, H[,vname])
-      }
-   
-      if (vname %in% c("sdSpatial") ) {
-        datarange = seq(0.1, 7, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "sdSpatial", sep=".")
-        annot = paste("Temperature bottom sdSpatial\n",  sep="")
-        xyz=cbind(loc, H[,vname])
-     }
-
-      if (vname %in% c("sdObs") ) {
-        datarange = seq(0, 4, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "sdObs", sep=".")
-        annot = paste("Temperature bottom sdObs\n",  sep="")
-        xyz=cbind(loc, (H[,vname]))
-      }
-
-      if (vname %in% c("range") ) {
-        datarange = seq( log(0.5), log(500), length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "range", sep=".")
-        annot = paste("Temperature bottom range\n",  sep="")
-        xyz=cbind(loc, log(H[,vname]))
-        xyz = xyz[which( is.finite(rowSums(xyz))),]
-      }
-
-      if (vname %in% c("phi") ) {
-        datarange = seq(log(0.15), log(100), length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "phi", sep=".")
-        annot = paste("Temperature bottom phi\n",  sep="")
-        xyz=cbind(loc, log(H[,vname]))
-        xyz = xyz[which( is.finite(rowSums(xyz))),]
-      }
-
-      if (vname %in% c("nu") ) {
-        datarange = seq(0.1, 3.5, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "nu", sep=".")
-        annot = paste("Temperature bottom nu\n",  sep="")
-        xyz=cbind(loc, (H[,vname]))
-      }
-
-      if (vname %in% c("ar_timerange") ) {
-        datarange = seq(0, 6, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "ar_timerange", sep=".")
-        annot = paste("Temperature bottom ar_timerange\n",  sep="")
-        xyz=cbind(loc, (H[,vname]))
-      }
-
-      if (vname %in% c("ar_1") ) {
-        datarange = seq(0, 1, length.out=100)
-        cols = color.code( "blue.black", datarange )
-        outfn = paste( "ar_1", sep=".")
-        annot = paste("Temperature bottom ar_1\n",  sep="")
-        xyz=cbind(loc, H[,vname])
-      }
-
-      bio.spacetime::map( xyz=xyz, cfa.regions=F, depthcontours=T, pts=NULL, annot=annot,
-        fn=outfn, loc=bottomdir.maps, at=datarange , col.regions=cols,
-        corners=p$corners, spatial.domain=p$spatial.domain ) 
-      print( file.path( bottomdir.maps,outfn))
-
-    }
-
-  }
-
 
 
   return (NULL)
