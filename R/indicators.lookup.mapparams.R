4e4157cb51959d14d8efa66b9a8373af4b688541
Jae S Choi <choijaeseok@gmail.com>
2017-02-01 12:58:59 -0400

    misc

--- R/indicators.lookup.mapparams.R
+++ R/indicators.lookup.mapparams.R
@@ -7,10 +7,10 @@ indicators.lookup.mapparams = function( DS="datarange", vn ){
     if ( vn == "ca2" ) datarange = seq(-2, 2, length.out=100)
     if ( vn == "pca1" ) datarange = seq(-2, 2, length.out=100)
     if ( vn == "pca2" ) datarange = seq(-2, 2, length.out=100)
-    if ( grepl("rsquared", vn, ignore.case=TRUE) ) datarange = seq(0, 1, length.out=100)
-    if ( grepl("phi", vn) ) datarange = seq(1, 75, length.out=100)
-    if ( grepl("range", vn) ) datarange = seq(1, 200, length.out=100)
-    if ( grepl("nu", vn) ) datarange = seq(0.1, 4, length.out=100)
+    if ( grepl("rsquared", vn, ignore.case=TRUE) ) datarange = seq(0.5, 0.95, length.out=100)
+    if ( grepl("phi", vn) ) datarange = seq(0.5, 50, length.out=100)
+    if ( grepl("range", vn) ) datarange = seq(0.5, 100, length.out=100)
+    if ( grepl("nu", vn) ) datarange = seq(0.1, 3, length.out=100)
     return(datarange)
   }
 
--- R/indicators.map.R
+++ R/indicators.map.R
@@ -8,8 +8,6 @@ indicators.map = function( ip=NULL, p=NULL, type="all", voi=NULL ) {
   
   if (is.null(voi)) if (exists("variables",p)) if(exists("Y", p$variables)) voi=p$variables$Y
    
-  loc = bathymetry.db(p=p, DS="baseline" )
-
   require( lattice )
 
   # -----------------------
@@ -34,18 +32,21 @@ indicators.map = function( ip=NULL, p=NULL, type="all", voi=NULL ) {
   if ( type %in% c("annual" ) ) {
     projectdir = file.path(p$project.root, "maps", voi, p$spatial.domain, "annual" )
     dir.create( projectdir, recursive=T, showWarnings=F )
-    
+      
+    loc = bathymetry.db(p=p, DS="baseline" )
+
+
     for (iy in ip ) {
       y = p$runs[iy, "yrs"]
       print(y)
       H = indicators.db( p=p, DS="predictions", year=y, ret="mean" )
       if (is.null(H)) next ()
-      xyz = cbind(loc, H[,iy])
+      xyz = cbind(loc, H[iy])
       uu = which( is.finite(rowSums(xyz)))
       if (length(uu) < 10) next()
       xyz = xyz[uu,]
       datarange = indicators.lookup.mapparams( DS="datarange", voi ) # hardcoded data ranges 
-      if (is.null(datarange)) datarange=quantile(xyz[,3], probs=c(0.05,0.95), na.rm=TRUE) 
+      if (is.null(datarange)) datarange=quantile(xyz[,3], probs=c(0.1,0.9), na.rm=TRUE) 
       cols = color.code( "blue.black", datarange )
       annot = gsub( ".", " ", toupper(voi), fixed=TRUE )
       outfn = paste( voi, "mean", y, sep=".")
@@ -55,12 +56,12 @@ indicators.map = function( ip=NULL, p=NULL, type="all", voi=NULL ) {
 
       H = indicators.db( p=p, DS="predictions", year=y, ret="sd" )
       if (is.null(H)) next ()
-      xyz = cbind(loc, H[,iy])
+      xyz = cbind(loc, H[iy])
       uu = which( is.finite(rowSums(xyz)))
       if (length(uu) < 10) next()
       xyz = xyz[uu,]
       datarange = indicators.lookup.mapparams( DS="datarange", voi ) # hardcoded data ranges 
-      if (is.null(datarange)) datarange=quantile(xyz[,3], probs=c(0.05,0.95), na.rm=TRUE) 
+      if (is.null(datarange)) datarange=quantile(xyz[,3], probs=c(0.1,0.9), na.rm=TRUE) 
       cols = color.code( "blue.black", datarange )
       annot = gsub( ".", " ", toupper(voi), fixed=TRUE )
       outfn = paste( voi, "sd", y, sep=".")
@@ -80,6 +81,9 @@ indicators.map = function( ip=NULL, p=NULL, type="all", voi=NULL ) {
   if ( type %in% c("climatology" ) ) {
     projectdir = file.path(p$project.root, "maps", voi, p$spatial.domain, "climatology" )
     dir.create( projectdir, recursive=T, showWarnings=F )
+    
+    loc = bathymetry.db(p=p, DS="baseline" )
+
     H = indicators.db( p=p, DS="complete" )
     vnames = setdiff( names(H), c("plon", "plat" ))
     
@@ -88,8 +92,9 @@ indicators.map = function( ip=NULL, p=NULL, type="all", voi=NULL ) {
       uu = which( is.finite(rowSums(xyz)))
       if (length(uu) < 10) next()
       xyz = xyz[uu,]
+      datarange= NULL
       datarange = indicators.lookup.mapparams( DS="datarange", vn) # hardcoded data ranges 
-      if (is.null(datarange)) datarange=quantile(xyz[,3], probs=c(0.05,0.95), na.rm=TRUE) 
+      if (is.null(datarange)) datarange=quantile(xyz[,3], probs=c(0.1,0.90), na.rm=TRUE) 
       cols = color.code( "blue.black", datarange )
       annot = gsub( ".", " ", toupper(vn), fixed=TRUE )
       bio.spacetime::map( xyz=xyz, cfa.regions=FALSE, depthcontours=TRUE, pts=NULL, 
