1236fa7c547c49df37784852d596da71199d0802
Jae S Choi <choijaeseok@gmail.com>
2017-02-01 12:20:58 -0400

    misc

--- R/indicators.lookup.mapparams.R
+++ R/indicators.lookup.mapparams.R
@@ -3,10 +3,14 @@ indicators.lookup.mapparams = function( DS="datarange", vn ){
 
   if (DS=="datarange") {
     datarange = NULL
-    if ( grepl("ca1", vn) ) datarange = seq(-2, 2, length.out=100)
-    if ( grepl("ca2", vn) ) datarange = seq(-2, 2, length.out=100)
-    if ( grepl("pca1", vn) ) datarange = seq(-2, 2, length.out=100)
-    if ( grepl("pca2", vn) ) datarange = seq(-2, 2, length.out=100)
+    if ( vn == "ca1" ) datarange = seq(-2, 2, length.out=100)
+    if ( vn == "ca2" ) datarange = seq(-2, 2, length.out=100)
+    if ( vn == "pca1" ) datarange = seq(-2, 2, length.out=100)
+    if ( vn == "pca2" ) datarange = seq(-2, 2, length.out=100)
+    if ( grepl("rsquared", vn, ignore.case=TRUE) ) datarange = seq(0, 1, length.out=100)
+    if ( grepl("phi", vn) ) datarange = seq(1, 75, length.out=100)
+    if ( grepl("range", vn) ) datarange = seq(1, 200, length.out=100)
+    if ( grepl("nu", vn) ) datarange = seq(0.1, 4, length.out=100)
     return(datarange)
   }
 
