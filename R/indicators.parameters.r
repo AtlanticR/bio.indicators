5182307e925c8744d88a329ee43cbe06688c377e
Jae S Choi <choijaeseok@gmail.com>
2017-02-09 12:25:34 -0400

    tweak

--- R/indicators.parameters.r
+++ R/indicators.parameters.r
@@ -266,7 +266,8 @@ indicators.parameters = function( p=NULL, DS="default", current.year=NULL, varna
       Y = varname, 
       LOCS = c("plon", "plat"), 
       TIME = "tiyr", 
-      COV = c("z", "dZ", "ddZ", "log.substrate.grainsize", "t", "tsd", "b.range", "s.range", "t.range" ) )
+      COV = c("z", "dZ", "ddZ", "log.substrate.grainsize", "t", "tsd.climatology", , "tmean.climatology",
+              "b.range", "s.range", "t.range" ) )
     p$varnames = c( p$variables$LOCS, p$variables$COV ) 
     
     if (!exists("lbm_variogram_method", p)) p$lbm_variogram_method = "fast"
@@ -276,9 +277,9 @@ indicators.parameters = function( p=NULL, DS="default", current.year=NULL, varna
     # using covariates as a first pass essentially makes it ~ kriging with external drift 
     # .. no space or time here .. only in the local model
     if (!exists("lbm_global_modelformula", p))  p$lbm_global_modelformula = formula( paste( 
-      varname, '~ s(t, bs="ts") + s(tsd, bs="ts") ',
-      ' + s(t.range, bs="ts") + s(b.range, bs="ts") + s(s.range, bs="ts")', 
-      ' + s(z, bs="ts") + s(dZ, bs="ts") + s(ddZ, bs="ts")  + s(log.substrate.grainsize, bs="ts") ' )) 
+      varname, '~ s(t, bs="ts") + s(tsd.climatology, bs="ts") + s(tmean.climatology, bs="ts") ',
+      ' + s(log(t.range), bs="ts") + s( log(b.range), bs="ts") + s( log(s.range), bs="ts")', 
+      ' + s(log(z), bs="ts") + s( log(dZ), bs="ts") + s( log(ddZ), bs="ts")  + s(log.substrate.grainsize, bs="ts") ' )) 
 
     if (!exists("lbm_global_family", p)) p$lbm_global_family = gaussian()
     if (!exists("lbm_local_family", p)) p$lbm_local_family = gaussian()
