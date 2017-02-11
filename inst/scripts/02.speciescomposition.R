adc26a17747763c4faef1b50dafb558c9d76e26a
Jae S Choi <choijaeseok@gmail.com>
2017-02-11 16:33:14 -0400

    updates

--- inst/scripts/02.speciescomposition.R
+++ inst/scripts/02.speciescomposition.R
@@ -23,10 +23,10 @@
     lbm( p=p, DATA=DATA, tasks=c("initiate", "globalmodel") ) # 5 min
     # lbm( p=p, tasks=c( "stage0" ) ) # serial mode
     # lbm( p=p, tasks=c( "continue" ) )    
-    lbm( p=p, tasks=c( "stage1" ) ) #  8 hrs 
-    lbm( p=p, tasks=c( "stage2" ) ) #  1 hrs
+    lbm( p=p, tasks=c( "stage1" ) ) #  ~1 hrs (1999-2016) 
+    lbm( p=p, tasks=c( "stage2" ) ) #  < 1 hrs
     lbm( p=p, tasks=c( "save" ) )
-    
+    NULL
 
     p = bio.indicators::indicators.parameters( DS="default", current.year=current.year )
     p = bio.indicators::indicators.parameters( p=p, DS="speciescomposition"  )
@@ -35,8 +35,8 @@
     p = make.list( list( yrs=p$yrs), Y=p )
     parallel.run( indicators.db, p=p, DS="predictions.redo" ) # warp predictions to other grids
     indicators.db( p=p, DS="lbm.stats.redo" ) # warp stats to other grids
-    indicators.db ( p=p, DS="complete.redo" )
-    indicators.db ( p=p, DS="baseline.redo" )
+    indicators.db( p=p, DS="complete.redo" )
+    indicators.db( p=p, DS="baseline.redo" )
     indicators.map( p=p )
 
     if (0) {
@@ -47,41 +47,43 @@
   }
 
 
+# ----------------
+
+ca1 :
+
 Family: gaussian 
 Link function: identity 
 
 Formula:
-ca1 ~ s(t, bs = "ts") + s(tsd.climatology, bs = "ts") + s(tmean.climatology, 
-    bs = "ts") + s(log(t.range), bs = "ts") + s(log(b.range), 
-    bs = "ts") + s(log(s.range), bs = "ts") + s(log(z), bs = "ts") + 
-    s(log(dZ), bs = "ts") + s(log(ddZ), bs = "ts") + s(log.substrate.grainsize, 
-    bs = "ts")
+ca1 ~ s(t, k = 3, bs = "ts") + s(tsd.climatology, k = 3, bs = "ts") + 
+    s(tmean.climatology, k = 3, bs = "ts") + s(log(t.range), 
+    k = 3, bs = "ts") + s(log(b.range), k = 3, bs = "ts") + s(log(z), 
+    k = 3, bs = "ts") + s(log(dZ), k = 3, bs = "ts") + s(log(ddZ), 
+    k = 3, bs = "ts") + s(log.substrate.grainsize, k = 3, bs = "ts")
 
 Parametric coefficients:
             Estimate Std. Error t value Pr(>|t|)    
-(Intercept) 0.104507   0.005456   19.16   <2e-16 ***
+(Intercept) 0.025523   0.004243   6.015 1.83e-09 ***
 ---
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 Approximate significance of smooth terms:
                              edf Ref.df       F  p-value    
-s(t)                       7.511      9  70.352  < 2e-16 ***
-s(tsd.climatology)         7.102      9   4.182 9.33e-07 ***
-s(tmean.climatology)       6.270      9 679.206  < 2e-16 ***
-s(log(t.range))            5.772      9  11.599  < 2e-16 ***
-s(log(b.range))            6.752      9   2.072 0.004846 ** 
-s(log(s.range))            6.414      9   2.832 0.000149 ***
-s(log(z))                  8.787      9  58.253  < 2e-16 ***
-s(log(dZ))                 8.438      9   3.902 2.05e-05 ***
-s(log(ddZ))                2.645      9   4.714 5.81e-11 ***
-s(log.substrate.grainsize) 8.393      9   7.882 1.22e-12 ***
+s(t)                       1.998      2  274.88  < 2e-16 ***
+s(tsd.climatology)         1.976      2   77.31  < 2e-16 ***
+s(tmean.climatology)       2.000      2 7016.86  < 2e-16 ***
+s(log(t.range))            1.396      2   68.72  < 2e-16 ***
+s(log(b.range))            1.969      2   21.13 4.59e-10 ***
+s(log(z))                  1.983      2  112.45  < 2e-16 ***
+s(log(dZ))                 1.888      2   14.47 1.98e-07 ***
+s(log(ddZ))                1.856      2   12.15 2.05e-06 ***
+s(log.substrate.grainsize) 1.752      2  274.56  < 2e-16 ***
 ---
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-R-sq.(adj) =  0.582   Deviance explained = 58.4%
-GCV = 0.37548  Scale est. = 0.37341   n = 12545
+R-sq.(adj) =  0.642   Deviance explained = 64.2%
+GCV = 0.37765  Scale est. = 0.37733   n = 20956
 ---
-
 
 
 # -------------------
@@ -91,34 +93,34 @@ Family: gaussian
 Link function: identity 
 
 Formula:
-ca2 ~ s(yr) + s(dyear, k = 3, bs = "ts") + s(yr, dyear, k = 36, 
-    bs = "ts") + s(t, bs = "ts") + s(tmean, bs = "ts") + s(tamplitude, 
-    bs = "ts") + s(z, bs = "ts") + s(dZ, bs = "ts") + s(ddZ, 
-    bs = "ts") + s(log.substrate.grainsize, bs = "ts")
+ca2 ~ s(t, k = 3, bs = "ts") + s(tsd.climatology, k = 3, bs = "ts") + 
+    s(tmean.climatology, k = 3, bs = "ts") + s(log(t.range), 
+    k = 3, bs = "ts") + s(log(b.range), k = 3, bs = "ts") + s(log(z), 
+    k = 3, bs = "ts") + s(log(dZ), k = 3, bs = "ts") + s(log(ddZ), 
+    k = 3, bs = "ts") + s(log.substrate.grainsize, k = 3, bs = "ts")
 
 Parametric coefficients:
              Estimate Std. Error t value Pr(>|t|)    
-(Intercept) -0.035536   0.002986   -11.9   <2e-16 ***
+(Intercept) -0.035122   0.003606   -9.74   <2e-16 ***
 ---
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
 Approximate significance of smooth terms:
-                              edf Ref.df        F  p-value    
-s(yr)                       1.529  1.886    1.623    0.154    
-s(dyear)                    1.994  2.000 1564.091  < 2e-16 ***
-s(yr,dyear)                32.999 33.000    6.903  < 2e-16 ***
-s(t)                        8.469  9.000   81.878  < 2e-16 ***
-s(tmean)                    8.284  9.000   41.237  < 2e-16 ***
-s(tamplitude)               5.023  9.000   43.472  < 2e-16 ***
-s(z)                        8.860  9.000 3456.474  < 2e-16 ***
-s(dZ)                       6.892  9.000    4.974 1.94e-08 ***
-s(ddZ)                      3.144  9.000    4.577 5.18e-10 ***
-s(log.substrate.grainsize)  8.178  9.000   59.988  < 2e-16 ***
+                               edf Ref.df        F  p-value    
+s(t)                       1.91152      2  427.472  < 2e-16 ***
+s(tsd.climatology)         1.99547      2  685.540  < 2e-16 ***
+s(tmean.climatology)       2.00000      2  103.914  < 2e-16 ***
+s(log(t.range))            1.99266      2   85.912  < 2e-16 ***
+s(log(b.range))            1.59609      2   15.951 9.64e-09 ***
+s(log(z))                  1.99890      2 4905.432  < 2e-16 ***
+s(log(dZ))                 0.09956      2    0.051    0.311    
+s(log(ddZ))                1.98690      2   31.422 7.97e-15 ***
+s(log.substrate.grainsize) 1.99396      2  168.870  < 2e-16 ***
 ---
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-R-sq.(adj) =  0.816   Deviance explained = 81.6%
-GCV = 0.18785  Scale est. = 0.18707   n = 20979
+R-sq.(adj) =  0.731   Deviance explained = 73.1%
+GCV = 0.27272  Scale est. = 0.2725    n = 20956
 ---
 
 
